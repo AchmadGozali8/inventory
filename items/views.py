33c2e742e59d30b116acb758002f0bc5b2591180
AchmadGozali8 <achmadg6@gmail.com>
2017-08-07 11:53:39 +0700

    add update and delete handler

--- items/views.py
+++ items/views.py
@@ -28,3 +28,33 @@ def add_item(request):
         }
 
     return render(request, 'items_form.html', context)
+
+def update_item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    if request.method == "POST":
+        form = ItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inventory/')
+    else:
+        form = ItemForm()
+        context = {
+            "form": form,
+            "pk": pk,
+        }
+
+    return render(request, 'item_update.html', context)
+
+def delete_item(request, pk):
+    deleted = 1
+    not_deleted = 0
+
+    item = get_object_or_404(Item, pk=pk)
+
+    if item.deleted == not_deleted:
+        post.deleted = deleted
+
+    item.save()
+
+    return HttpResponseRedirect('/inventory/')
