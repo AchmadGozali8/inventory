ea0a718b5725d648085b2c50fd2abadd2d137dfd
AchmadGozali8 <achmadg6@gmail.com>
2017-08-07 09:13:01 +0700

    add description

--- items/form.py
+++ items/form.py
@@ -1,16 +1,17 @@
-from django.forms import ModelForm, TextInput, Textare 
+from django.forms import ModelForm, TextInput, Textarea, Select 
 from models import Item
 
 class ItemForm(ModelForm):
     class Meta:
         model = Item
-        fields = ('name', 'type', 'quantity', 'description', 'location')
-        text_input = TextInput(attrs={'class': 'form-input', 'type': 'text})'
+        fields = ('name', 'types', 'quantity', 'description', 'location')
+        text_input = TextInput(attrs={'class': 'form-control', 'type': 'text'})
         widgets = {
             'name': text_input,
-            'type': Select(attrs={'class': 'form-control show-tick'}),
+            'types': Select(attrs={'class': 'form-control show-tick'}),
             'quantity': text_input,
             'location': text_input,
-            'description': TextArea(attrs={'cols'="30", 'rows'="5", \
-                                           'class'="form-control no-resize"}
+            'description': Textarea(attrs={'cols': "30", 'rows': "5",\
+                        'class': "form-control no-resize"})
+
         }
