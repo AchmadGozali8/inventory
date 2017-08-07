a8f34aba0a2b5a4ff1b3fe50a39d619d5efd7e5f
AchmadGozali8 <achmadg6@gmail.com>
2017-08-07 09:13:48 +0700

    add post handler

--- items/views.py
+++ items/views.py
@@ -3,16 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from models import Item
+from django.http import HttpResponseRedirect
+from form import ItemForm
 # Create your views here.
 
 def item(request):
     items = Item.status.all()
-
     context = {
         'items':items
     }
-
     return render(request, 'items.html', context)
 
 def add_item(request):
-    return render(request, 'items_form.html')
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return HttpResponseRedirect('/inventory/')
+    else:
+        form = ItemForm()
+        context = {
+            'form': form
+        }
+
+    return render(request, 'items_form.html', context)
