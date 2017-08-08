562e4b0a6ef2a0b8b8d90bc120569b2a4efd8e5c
AchmadGozali8 <achmadg6@gmail.com>
2017-08-08 07:28:54 +0700

    add placeholder each form wich will be updated

--- items/views.py
+++ items/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import Item
 from django.http import HttpResponseRedirect
-from form import ItemForm
+from form import ItemForm, ItemUpdateForm
 # Create your views here.
 
 def item(request):
@@ -31,30 +31,30 @@ def add_item(request):
 
 def update_item(request, pk):
     item = get_object_or_404(Item, pk=pk)
-
     if request.method == "POST":
-        form = ItemForm(request.POST, request.FILES, instance=item)
+        form = ItemUpdateForm(request.POST, request.FILES, instance=item)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/inventory/')
     else:
         form = ItemForm()
+        form.fields['name'].widget.attrs['placeholder'] = item.name
+        form.fields['types'].widget.attrs['placeholder'] = item.types
+        form.fields['description'].widget.attrs['placeholder'] = item.description
+        form.fields['location'].widget.attrs['placeholder'] = item.location 
         context = {
             "form": form,
             "pk": pk,
         }
 
-    return render(request, 'item_update.html', context)
+    return render(request, 'items_update.html', context)
 
 def delete_item(request, pk):
     deleted = 1
     not_deleted = 0
 
     item = get_object_or_404(Item, pk=pk)
-
-    if item.deleted == not_deleted:
-        post.deleted = deleted
-
+    item.deleted = deleted
     item.save()
 
     return HttpResponseRedirect('/inventory/')
