bc29a5cebd516e530eb0a4d48b83157a22f3ec79
AchmadGozali8 <achmadg6@gmail.com>
2017-08-08 07:26:15 +0700

    add update and deleted, url

--- items/urls.py
+++ items/urls.py
@@ -4,4 +4,6 @@ from items import views
 urlpatterns = [
     url(r'^$', views.item),
     url(r'^add/', views.add_item),
+    url(r'^delete/(?P<pk>\d+)/', views.delete_item),
+    url(r'^update/(?P<pk>\d+)/', views.update_item)
 ]
