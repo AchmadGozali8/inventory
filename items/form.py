5f7355632a594117b6082bd62f466dcdc1afaf2b
AchmadGozali8 <achmadg6@gmail.com>
2017-08-08 07:25:17 +0700

    add update form handler

--- items/form.py
+++ items/form.py
@@ -1,11 +1,12 @@
 from django.forms import ModelForm, TextInput, Textarea, Select 
 from models import Item
 
+text_input = TextInput(attrs={'class': 'form-control', 'type': 'text'})
+
 class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = ('name', 'types', 'quantity', 'description', 'location')
-        text_input = TextInput(attrs={'class': 'form-control', 'type': 'text'})
         widgets = {
             'name': text_input,
             'types': Select(attrs={'class': 'form-control show-tick'}),
@@ -15,3 +16,16 @@ class ItemForm(ModelForm):
                         'class': "form-control no-resize"})
 
         }
+
+class ItemUpdateForm(ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'types', 'description', 'location')
+        widgets = {
+            'name': text_input,
+            'types': Select(attrs={'class': 'form-control show-tick'}),
+            'description': Textarea(attrs={'cols': "30", 'rows': "5",\
+                        'class': "form-control no-resize"}),
+            'location': text_input
+        }
+        print ModelForm
